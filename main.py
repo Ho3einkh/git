0e45c979a14758af6a9e50c5b0be5c7dffea62fe
Ho3ein <hos.kh93@gmail.com>
2021-12-25 11:19:39 +0330

    new changes

---
+++ main.py
@@ -0,0 +1,5 @@
+def hi():
+    return "hello my name is admin"
+
+def bye():
+    return "Goodbye!"
--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("hello my name is admin")
+import main as m
+
+def test_hi():
+    if len(m.hi()) > 0:
+        print('Pass: hi function')
+    else:
+        assert False, 'hi function is not working'
+
+def test_bye():
+    if len(m.bye()) > 0:
+        print('Pass: bye function')
+    else:
+        assert False, 'bye function is not working'
+
+
+test_hi()
+test_bye()
